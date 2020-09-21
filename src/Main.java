34995a9f8e8fcce378d268e4d1589d70aec6f85f
Anitha <hello.ani25@gmail.com>
2020-09-20 22:55:29 -0400

    Removed IOException import

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,10 @@ import com.target.search.IndexedDocumentSearch;
 import com.target.search.RegexDocumentSearch;
 import com.target.search.SimpleDocumentSearch;
 
-import java.io.IOException;
-
 public class Main {
 
     public static void main(String... args) {
-        String phrase = "faster-than-light";
+        String phrase = "EVE";
         SimpleDocumentSearch simpleDocumentSearch = new SimpleDocumentSearch();
         simpleDocumentSearch.setUp();
         simpleDocumentSearch.getSearchResults(phrase);

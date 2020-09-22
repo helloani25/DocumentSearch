ed8f0ecb12d4529e56cdae608357e19647bf19c4
Anitha <hello.ani25@gmail.com>
2020-09-21 22:05:08 -0400

    Added thread pool executpr

--- test/com.target.search/Main.java
+++ test/com.target.search/Main.java
@@ -1,34 +1,19 @@
 package com.target.search;
 
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.List;
-
 public class Main {
     public static void main(String... args) {
         GeneratePhrases generatePhrases = new GeneratePhrases();
         generatePhrases.generateRandomPhrases();
-
-        try {
-            List<String> phrases = Files.readAllLines(Paths.get("resources/generate_phrases.txt"));
-            for (String phrase : phrases) {
-                System.out.println("******  phrase - " + phrase);
-                SimpleDocumentSearch simpleDocumentSearch = new SimpleDocumentSearch();
-                simpleDocumentSearch.setUp();
-                simpleDocumentSearch.getSearchResults(phrase);
-                RegexDocumentSearch regexDocumentSearch = new RegexDocumentSearch();
-                regexDocumentSearch.setup();
-                regexDocumentSearch.getSearchResults(phrase);
-                IndexedDocumentSearch indexedDocumentSearch = new IndexedDocumentSearch();
-                indexedDocumentSearch.setup();
-                indexedDocumentSearch.getSearchResults(phrase);
-            }
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
+        SimpleDocumentSearch simpleDocumentSearch = new SimpleDocumentSearch();
+        simpleDocumentSearch.setUp();
+        RegexDocumentSearch regexDocumentSearch = new RegexDocumentSearch();
+        regexDocumentSearch.setup();
+        IndexedDocumentSearch indexedDocumentSearch = new IndexedDocumentSearch();
+        indexedDocumentSearch.setup();
+        SearchPerformanceTest searchPerformanceTest = new SearchPerformanceTest();
+        searchPerformanceTest.executeSimpleSearch(simpleDocumentSearch);
+        searchPerformanceTest.executeRegexSearch(regexDocumentSearch);
+        searchPerformanceTest.executeIndexedSearch(indexedDocumentSearch);
     }
 }
 
--- test/com.target.search/SearchPerformanceTest.java
+++ test/com.target.search/SearchPerformanceTest.java
@@ -1,4 +1,78 @@
 package com.target.search;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class SearchPerformanceTest {
+
+    public void executeSimpleSearch(SimpleDocumentSearch simpleDocumentSearch) {
+
+        ExecutorService executor = Executors.newFixedThreadPool(6);
+        try {
+            List<String> phrases = Files.readAllLines(Paths.get("resources/generate_phrases.txt"));
+            for (String phrase : phrases) {
+                Runnable task = new Runnable() {
+                    @Override
+                    public void run() {
+                        System.out.println("******  PHRASE - " + phrase);
+                        simpleDocumentSearch.getSearchResults(phrase);
+                    }
+                };
+                executor.execute(task);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        executor.shutdown();
+    }
+
+    public void executeRegexSearch(RegexDocumentSearch regexDocumentSearch) {
+
+        ExecutorService executor = Executors.newFixedThreadPool(6);
+        try {
+            List<String> phrases = Files.readAllLines(Paths.get("resources/generate_phrases.txt"));
+            for (String phrase : phrases) {
+                Runnable task = new Runnable() {
+                    @Override
+                    public void run() {
+                        System.out.println("******  PHRASE - " + phrase);
+                        regexDocumentSearch.getSearchResults(phrase);
+                    }
+                };
+                executor.execute(task);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        executor.shutdown();
+    }
+
+    public void executeIndexedSearch(IndexedDocumentSearch indexedDocumentSearch) {
+
+        ExecutorService executor = Executors.newFixedThreadPool(6);
+        try {
+            List<String> phrases = Files.readAllLines(Paths.get("resources/generate_phrases.txt"));
+            for (String phrase : phrases) {
+                Runnable task = new Runnable() {
+                    @Override
+                    public void run() {
+                        System.out.println("******  PHRASE - " + phrase);
+                        indexedDocumentSearch.getSearchResults(phrase);
+                    }
+                };
+                executor.execute(task);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        executor.shutdown();
+    }
+
+
+
+
 }

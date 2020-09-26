3ca75cfa85456056d19938ba1eeb201310286243
Anitha <hello.ani25@gmail.com>
2020-09-26 10:56:22 -0400

    Added Constants for phrase and stopwords

---
+++ src/test/java/com/target/search/Constants.java
@@ -0,0 +1,6 @@
+package com.target.search;
+
+public class Constants {
+    public static String PHRASE_FIILE = "src/main/resources/generate_phrases.txt";
+    public static String STOPWORDS_FILE = "src/main/resources/english_stopwords.txt";
+}

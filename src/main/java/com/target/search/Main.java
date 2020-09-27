1d45ec59b797706a1f9da69b5aae817980732fb7
Anitha <hello.ani25@gmail.com>
2020-09-27 11:49:02 -0400

    Fixed to read from Standard Input

--- src/main/java/com/target/search/Main.java
+++ src/main/java/com/target/search/Main.java
@@ -1,24 +1,44 @@
 package com.target.search;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String... args) {
-        String phrase = "Following defeat in the Franco-Prussian War, Franco-German rivalry erupted again in the First World War. France and its allies were victorious this time. Social, political, and economic upheaval in the wake of the conflict led to the Second World War, in which the Allies were defeated in the Battle of France and the French government surrendered and was replaced with an authoritarian regime. The Allies, including the government in exile's Free French Forces and later a liberated French nation, eventually emerged victorious over the Axis powers. As a result, France secured an occupation zone in Germany and a permanent seat on the United Nations Security Council. The imperative of avoiding a third Franco-German conflict on the scale of those of two world wars paved the way for European integration starting in the 1950s. France became a nuclear power and since the 1990s its military action is most often seen in cooperation with NATO and its European partners.";
-        SimpleDocumentSearch simpleDocumentSearch = new SimpleDocumentSearch();
-        simpleDocumentSearch.setUp();
-        simpleDocumentSearch.getSearchResults(phrase);
-        RegexDocumentSearch regexDocumentSearch = new RegexDocumentSearch();
-        regexDocumentSearch.setup();
-        regexDocumentSearch.getSearchResults(phrase);
-        IndexedDocumentSearch indexedDocumentSearch = new IndexedDocumentSearch();
-        try {
-            indexedDocumentSearch.setup();
-            indexedDocumentSearch.getSearchResults(phrase);
-        } catch (IOException e) {
-            e.printStackTrace();
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the search term:   ");
+        String phrase = scanner.nextLine();
+        System.out.println("1) String Match   2) Regular Expression    3) Indexed");
+        String searchMethod = scanner.nextLine();
+        switch (searchMethod) {
+            case "1":
+            case "String Match":
+                SimpleDocumentSearch simpleDocumentSearch = new SimpleDocumentSearch();
+                simpleDocumentSearch.setUp();
+                simpleDocumentSearch.getSearchResults(phrase);
+                break;
+            case "2":
+            case "Regular Expression":
+                RegexDocumentSearch regexDocumentSearch = new RegexDocumentSearch();
+                regexDocumentSearch.setup();
+                regexDocumentSearch.getSearchResults(phrase);
+                break;
+            case "3":
+            case "Indexed":
+                IndexedDocumentSearch indexedDocumentSearch = new IndexedDocumentSearch();
+                try {
+                    indexedDocumentSearch.setup();
+                    indexedDocumentSearch.getSearchResults(phrase);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                break;
+            default:
+                System.out.println("Invalid Entry");
+                break;
         }
+        scanner.close();
     }
 }
 
